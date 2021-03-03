8160eeffcf0f29a5f4d8dae5fa41c326abbbf138
moogaun <79950734+moogaun@users.noreply.github.com>
2021-03-03 11:02:29 +0900

    Add files via upload

---
+++ lllllll.java
@@ -0,0 +1,23 @@
+package moogaun;
+
+import java.util.Scanner;
+
+public class lllllll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+	    int[] arr = new int[10];
+	    
+	    Scanner sc = new Scanner(System.in);
+	    
+	    for(int i=0; i<5; i++) {
+	    	arr[i] = sc.nextInt();
+		   
+	    }
+	    
+	    for(int i=0;i<5; i++) {
+	    	System.out.println(arr[i]);
+	    }
+	}
+}
